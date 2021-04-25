93a7a3062c924de3e71480f12ae337d4ac5680a3
hs lee <dlgudtn032@naver.com>
2021-04-26 03:15:45 +0900

    for test

---
+++ index2.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// var getDaysInMonth = function(month,year) {
+//    return new Date(year, month, 0).getDate();
+//   };
+
+// const res = fetch('https://rest.dealink.co.kr/auction/group/2?page=0&size=3&sort=createdDate,desc',{
+// 	method:'GET'
+// })
+// .then(response=>response.json())
+// .then(result=>console.log(result[0].closingTime))
+// .catch(error=>Error(error))
+
+// let handling;
+
+// const seconds = setInterval (function increaseTimer(){
+// 	handling += 1000;
+// 	return handling;
+// },1000)
+
+const res = fetch('https://rest.dealink.co.kr/auction/group/2?page=0&size=3&sort=createdDate,desc',{
+	method:'GET'
+})
+.then(response=>response.json())
+.then(function(result){
+	for(const goodsId in result){
+		document.querySelector('.div' + goodsId).innerHTML = timer(getDistance(result[goodsId]));
+	}
+})
+.catch(error=>Error(error));
+
+
+function getDistance(time1){
+	 const time2 = new Date().toISOString().substr(0, 19);
+	 const distance = new Date(time1.closingTime).getTime() - new Date(time2).getTime();
+	 return distance;
+	};
+
+// getDistance와 timer를 분리시킬 필요가 있음
+
+function timer (distance){
+	const day = Math.floor(distance/(1000*60*60*24));
+	const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+	const minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+	const seconds = Math.floor((distance % (1000*60))/1000);
+
+	return distance<=0? "경매 종료" : 
+	`D-${day} ${hours < 10 ? `0${hours}` : hours}:
+	${minutes < 10 ? `0${minutes}` : minutes}:
+	${seconds < 10 ? `0${seconds}` : seconds}남음`;
+}   ;
